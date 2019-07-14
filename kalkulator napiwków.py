f5f28735d10fee41bf3991f15969727db1d7c4a6
Kamil Ziółkowski <50667076+kamilpoyel@users.noreply.github.com>
2019-07-14 21:46:13 +0200

    Add files via upload
    Prosty program do obliczania napiwków

---
+++ kalkulator napiwków.py
@@ -0,0 +1,17 @@
+q='Kalkulator napiwków'
+print(q.upper())
+
+print('\nJeśli chcesz dobrze sie zachowć w restauracji zostaw napiwek')
+print('W zależności od kraju sa to wysokości rzedu:')
+print('\t\t 15%')
+print('\t\t 20%')
+
+x=int(input('Jeśli chcesz wiedzieć ile powinienieś zostawić napiwku w danych krajach wpisz kwotę jaką zapłaciłeś w restauracji'))
+print('W krajach AA:')
+y=(15/100)
+print(x*y)
+print('W krajach BB')
+z=(20/100)
+print(x*z)
+print('i wszystko jasne...')
+
